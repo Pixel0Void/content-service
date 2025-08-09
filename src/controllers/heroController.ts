11cb341f6826a7a20de4ccd77b354760c11eb4e1
PixelVoid <Pixel0Void@gmail.com>
2025-08-09 11:05:23 +0430

    implement get hero by id function

--- src/controllers/heroController.ts
+++ src/controllers/heroController.ts
@@ -16,7 +16,18 @@ export const getUpdates = async (req: Request, res: Response) => {
 };
 
 export const getHeroById = async (req: Request, res: Response) => {
-    
+    try {
+        const heroId = req.params.id;
+        const hero = await Hero.findById(heroId);
+        if (!hero) {
+            return res.status(404).json({ message: 'Hero not found.' });
+        }
+
+        return res.status(200).json(hero);
+    } catch (error: any) {
+        console.error('Error fetching hero by ID: ', error);
+        return res.status(500).json({ message: 'Server error while fetching heroes.', error: error.message });
+    }
 };
 
 export const insertNewHero = async (req: Request, res: Response) => {
