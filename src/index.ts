55a0cc7f30df661ecb7d551681889d87d0a20edb
PixelVoid <Pixel0Void@gmail.com>
2025-08-09 10:31:51 +0430

    database connection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import { config } from './config/index';
 
 import dotenv from 'dotenv';
@@ -6,10 +7,16 @@ dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send(200).send('Content Service is running!');
+mongoose.connect(config.mongoURI).then(() => {
+    console.log('Connected to MongoDB!');
+    app.listen(config.port, () => {
+        console.log(`Content Service is running on port ${config.port}.`);
+    });
+}).catch(err => {
+    console.error('MongoDB connection error: ', err);
+    process.exit(1);
 });
 
-app.listen(config.port, () => {
-    console.log(`Content Service is running on port ${config.port}.`);
+app.get('/', (req, res) => {
+    res.send(200).send('Content Service is running!');
 });
