a1cfaedce7c5b393e34e5d30ced46971f166cc50
PixelVoid <Pixel0Void@gmail.com>
2025-08-09 10:56:11 +0430

    define routes

---
+++ src/controllers/heroController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+export const getAllHeroes = async (req: Request, res: Response) => {
+    
+};
+
+export const getUpdates = async (req: Request, res: Response) => {
+    
+};
+
+export const getHeroById = async (req: Request, res: Response) => {
+    
+};
+
+export const insertNewHero = async (req: Request, res: Response) => {
+    
+};
+
+export const updateHero = async (req: Request, res: Response) => {
+    
+};
