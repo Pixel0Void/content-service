446ee89af54d6c85d6215fc31c5794431600e154
PixelVoid <Pixel0Void@gmail.com>
2025-08-09 11:33:26 +0430

    add sample data for heroes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import heroRoutes from './routes/heroRoutes';
 import { config } from './config/index';
 
 import dotenv from 'dotenv';
+import { insertSampleData } from './utils/sampleHeroData';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use('/heroes', heroRoutes);
 
 mongoose.connect(config.mongoURI).then(() => {
     console.log('Connected to MongoDB!');
     app.listen(config.port, () => {
         console.log(`Content Service is running on port ${config.port}.`);
+        insertSampleData();
     });
 }).catch(err => {
     console.error('MongoDB connection error: ', err);
